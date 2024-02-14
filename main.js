12dff81a8da1b46b9285807e00b5a7f8001e88ae
SFaucher26 <sandrine.faucher@maif.fr>
2024-02-14 17:03:00 +0100

    ajout d'une boucle qui s'arrête aléatoirement

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 let div = document.querySelectorAll("div");
 let btns = document.querySelectorAll("button");
 let btn = document.querySelector("#toggle");
+const delay = Math.floor(Math.random() * 5000);
+console.log(delay);
+
+console.log(delay);
 
 const addClass = () => {
   for (let i = 0; i < div.length; i++) {
@@ -38,28 +42,37 @@ const next = () => {
   }
 };
 
-const prev = () => {
-  for (i = 0; i < div.length; i++) {
-    const element = div[i];
-    const elementPrev = element.previousElementSibling;
-    if (
-      element.classList.contains("active") &&
-      elementPrev.classList.contains("box")
-    ) {
-      removeClass();
-      elementPrev.classList.add("active");
-    }
-  }
-};
+// const prev = () => {
+//   for (i = 0; i < div.length; i++) {
+//     const element = div[i];
+//     const elementPrev = element.previousElementSibling;
+//     if (
+//       element.classList.contains("active") &&
+//       elementPrev.classList.contains("box")
+//     ) {
+//       removeClass();
+//       elementPrev.classList.add("active");
+//     }
+//   }
+// };
 
 btns.forEach((element) => {
   element.addEventListener("click", () => {
-    if (element.id == "prev") {
-      prev();
-    }
+    // if (element.id == "prev") {
+    //     prev();
+    // }
 
     if (element.id == "next") {
-      next();
+      //   next();
+      // Se répète toutes les 2 secondes
+      let boucle = setInterval(next, 500);
+
+      // S'arrête après le temps de la variable delay qui est un Math.random
+      setTimeout(() => {
+        clearInterval(boucle);
+      }, delay);
+
+    
     }
     if (element.id == "toggle") {
       toggleClass();
