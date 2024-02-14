5dd99dcde2aeece367f452f110df9f0c68a10621
SFaucher26 <sandrine.faucher@maif.fr>
2024-02-14 12:44:00 +0100

    ajout fonction prev et next

--- main.js
+++ main.js
@@ -1,46 +1,68 @@
-let div = document.querySelectorAll('div');
-let btns = document.querySelectorAll('button');
+let div = document.querySelectorAll("div");
+let btns = document.querySelectorAll("button");
 let btn = document.querySelector("#toggle");
 
 const addClass = () => {
-    for(let i = 0; i < div.length ; i++){
-        const element = div[i];
-        element.classList.add("active");
-    }
-}
+  for (let i = 0; i < div.length; i++) {
+    const element = div[i];
+    element.classList.add("active");
+  }
+};
 
 const removeClass = () => {
-    for(i = 0; i < div.length ; i++){
-        const element = div[i];
-        element.classList.remove("active");
+  for (i = 0; i < div.length; i++) {
+    const element = div[i];
+    element.classList.remove("active");
+  }
+};
+
+const toggleClass = () => {
+  for (i = 0; i < div.length; i++) {
+    const element = div[i];
+    element.classList.toggle("active");
+  }
+};
+
+const next = () => {
+  for (i = 0; i < div.length; i++) {
+    const element = div[i];
+    const elementNext = element.nextElementSibling;
+    if (element.classList.contains("active")) {
+      removeClass();
+      if (elementNext.classList.contains("box")) {
+        elementNext.classList.add("active");
+      } else {
+        div[0].classList.add("active");
+      }
     }
-}
+  }
+};
 
-const toggleClass = () =>{
-    for(i = 0; i < div.length ; i++){
-        const element = div[i];
-        element.classList.toggle("active");
+const prev = () => {
+  for (i = 0; i < div.length; i++) {
+    const element = div[i];
+    const elementPrev = element.previousElementSibling;
+    if (
+      element.classList.contains("active") &&
+      elementPrev.classList.contains("box")
+    ) {
+      removeClass();
+      elementPrev.classList.add("active");
     }
-}
+  }
+};
 
-const prev = () =>{
-    for(i = 0; i < div.length ; i++){
-        const element = div[i];
-        
+btns.forEach((element) => {
+  element.addEventListener("click", () => {
+    if (element.id == "prev") {
+      prev();
     }
-}
 
-
-btns.forEach(element =>{   
-    element.addEventListener("click", () => {
-        if(element.id == "prev"){  
-             addClass();
-        };
-        if(element.id == "next"){
-            removeClass();
-        };
-        if(element.id == "toggle"){
-            toggleClass()
-        }
-    })
-})
+    if (element.id == "next") {
+      next();
+    }
+    if (element.id == "toggle") {
+      toggleClass();
+    }
+  });
+});
